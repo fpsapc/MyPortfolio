6818433b984932c2ef0991f3b6c5fa01f28bb439
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-03-08 14:31:15 +0500

    Add pop-up functionality but need some fix

--- js/script.js
+++ js/script.js
@@ -94,10 +94,67 @@ projects.map((card) => {
               <p class="flex-item item1 proitem1">${card.technologies[2]}</p>
             </div>
           </div>
-          <button class="work-button probutton">
+          <button class="work-button probutton ">
           ${card.seeProject}
           </button>
   </div>
   `;
   return ('');
+});
+
+// pop-up section
+const seeProject = document.querySelectorAll('.work-button');
+const popMenu = document.querySelector('project-popup');
+
+seeProject.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    popMenu.classList.add('act');
+    popMenu.innerHTML = `    
+    <section id="popup" class="popup">
+        <div id="popup-title">
+          <h3 class="popup-heading flex">${projects[index].name}<h3>
+            <span class="close-btn">&#x2715;</span>
+            <div class="flex
+            flex-start">
+              <p class="flex-item 
+                  item1">${projects[index].technologies[0]}
+              </p>
+              <p class="flex-item 
+                  item2">${projects[index].technologies[1]}
+              </p>
+              <p class="flex-item 
+                item3">${projects[index].technologies[2]}
+              </p>
+              <p class="flex-item 
+                item4">${projects[index].technologies[3]}
+              </p>
+          </div>
+        </div>
+          <div class="popup-card">
+             <div id="popup-image">
+              </div>
+              <div class="flex flex-column">
+                <p class="popup-details">
+                ${projects[index].description}
+                </p>
+                <div class="flex popup-textbtn">
+                  <button id= "see-live" 
+                  class="work-button">
+                  ${projects[index].linkLive} 
+                  <img src="/img/live.png" alt="live icon">
+                  </button>
+                  <button id= "see-source" 
+                  class="work-button">
+                  ${projects[index].linkSource} <img src="/img/project.png" alt="source icon">
+                  </button>
+                </div>
+              </div>
+          </div>
+     </section>
+`;
+    const popClose = document.querySelector('.close-btn');
+    popClose.addEventListener('click', () => {
+      popMenu.classList.remove('act');
+    });
+  });
 });
