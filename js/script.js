564cccb64b6d8e5d1b30aa8bb555d1a2a3269459
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-03-08 19:29:38 +0500

    Add all functionaliies

--- js/script.js
+++ js/script.js
@@ -13,13 +13,12 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   document.body.classList.remove('no-scroll');
 }));
 
-// Project Section
 const projects = [
   {
     id: 'bg-pro1',
     name: 'Professional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: 'Img Placeholder1-3.svg',
+    featuredImage: '/img/Img Placeholder1-3.svg',
     technologies: ['html', 'Bootstrap', 'Ruby'],
     seeProject: 'See Project',
     linkLive: 'See live',
@@ -29,7 +28,7 @@ const projects = [
     id: 'bg-pro2',
     name: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: '../img/Img Placeholder1-2.svg',
+    featuredImage: '/img/Img Placeholder1-2.svg',
     technologies: ['html', 'Bootstrap', 'Ruby'],
     seeProject: 'See Project',
     linkLive: 'See live',
@@ -39,7 +38,7 @@ const projects = [
     id: 'bg-pro3',
     name: 'Website Protfolio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: '../img/Img Placeholder1-1.svg',
+    featuredImage: '/img/Img Placeholder1-1.svg',
     technologies: ['html', 'Bootstrap', 'Ruby'],
     seeProject: 'See Project',
     linkLive: 'See live',
@@ -49,7 +48,7 @@ const projects = [
     id: 'bg-pro4',
     name: 'Professional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: '../img/Img Placeholder1-3.svg',
+    featuredImage: '/img/Img Placeholder1-3.svg',
     technologies: ['html', 'Bootstrap', 'Ruby'],
     seeProject: 'See Project',
     linkLive: 'See live',
@@ -59,7 +58,7 @@ const projects = [
     id: 'bg-pro5',
     name: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: '../img/Img Placeholder1-2.svg',
+    featuredImage: '/img/Img Placeholder1-2.svg',
     technologies: ['html', 'Bootstrap', 'Ruby'],
     seeProject: 'See Project',
     linkLive: 'See live',
@@ -69,7 +68,7 @@ const projects = [
     id: 'bg-pro6',
     name: 'Website Protfolio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: '../img/Img Placeholder1-1.svg',
+    featuredImage: '/img/menu.png',
     technologies: ['html', 'Bootstrap', 'Ruby'],
     seeProject: 'See Project',
     linkLive: 'See live',
@@ -102,46 +101,117 @@ projects.map((card) => {
   return ('');
 });
 
+const allProjects = [
+  {
+    id: 'projects',
+    name: 'My Recent Works',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+  {
+    id: 'bg-pro1',
+    name: 'Professional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1-3.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+  {
+    id: 'bg-pro2',
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1-2.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+  {
+    id: 'bg-pro3',
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1-1.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+  {
+    id: 'bg-pro4',
+    name: 'Professional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1-3.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+  {
+    id: 'bg-pro5',
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1-2.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+  {
+    id: 'bg-pro6',
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '/img/Img Placeholder1-1.svg',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    seeProject: 'See Project',
+    linkLive: 'See live',
+    linkSource: 'See source',
+  },
+];
 // pop-up section
 const seeProject = document.querySelectorAll('.work-button');
 const popMenu = document.querySelector('.seePopup');
 
 seeProject.forEach((btn, index) => {
   btn.addEventListener('click', () => {
+    document.body.classList.toggle('no-scroll');
     popMenu.classList.add('act');
     popMenu.innerHTML = `    
     <section id="popup" class="popup">
         <div id="popup-title">
-          <h3 class="popup-heading flex">${projects[index].name}<h3>
+          <h3 class="popup-heading flex">${allProjects[index].name}<h3>
             <span class="close-btn">&#x2715;</span>
             <div class="flex
             flex-start">
-              <p class="flex-item">${projects[index].technologies[0]}
+              <p class="flex-item">${allProjects[index].technologies[0]}
               </p>
-              <p class="flex-item">${projects[index].technologies[1]}
+              <p class="flex-item">${allProjects[index].technologies[1]}
               </p>
-              <p class="flex-item">${projects[index].technologies[2]}
-              </p>
-              <p class="flex-item">${projects[index].technologies[3]}
+              <p class="flex-item">${allProjects[index].technologies[2]}
               </p>
           </div>
         </div>
           <div class="popup-card">
-             <div id="popup-image">${projects[index].featuredImage}
+             <div id="popup-image"><img src="${allProjects[index].featuredImage}" alt="something here">
               </div>
               <div class="flex flex-column">
                 <p class="popup-details">
-                ${projects[index].description}
+                ${allProjects[index].description}
                 </p>
                 <div class="flex popup-textbtn">
                   <button id= "see-live" 
-                  class="work-button">
-                  ${projects[index].linkLive} 
+                  class="work-button"><a href="https://fpsapc.github.io/MicroverseStudentProject1/">${allProjects[index].linkLive}</a>
                   <img src="/img/live.png" alt="live icon">
                   </button>
                   <button id= "see-source" 
                   class="work-button">
-                  ${projects[index].linkSource} <img src="/img/project.png" alt="source icon">
+                  <a href="https://github.com/fpsapc/MicroverseStudentProject1/">${allProjects[index].linkSource}</a>
+                   <img src="/img/project.png" alt="source icon">
                   </button>
                 </div>
               </div>
@@ -151,6 +221,7 @@ seeProject.forEach((btn, index) => {
     const popClose = document.querySelector('.close-btn');
     popClose.addEventListener('click', () => {
       popMenu.classList.remove('act');
+      document.body.classList.remove('no-scroll');
     });
   });
 });
